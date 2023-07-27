2b1afd2d1103570888211e14fe65bd476c30de37
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2023-07-27 22:27:54 +0200

    Refactor update method out

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"github.com/nrdcg/porkbun"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -67,23 +68,27 @@ func updateRecords(c configuration) {
 			log.Error(err)
 			continue
 		}
-		ipv4Record, ipv6Record, err := getRecords(ctx, record.Domain, record.Host, client)
-		if err != nil {
-			log.Error(err)
-		}
-		if record.IpV4 && ipv4Record != nil && ipv4address != "" && ipv4Record.Content != ipv4address {
-			err = createOrUpdateRecord(ctx, client, ipv4Record.ID, record.Domain, record.Host, "A", ipv4address)
-		} else {
-			log.Debugf("Ipv4 update not required for %s.%s", record.Host, record.Domain)
-		}
-		if record.IpV6 && ipv6Record != nil && ipv6address != "" && ipv6Record.Content != ipv6address {
-			err = createOrUpdateRecord(ctx, client, ipv6Record.ID, record.Domain, record.Host, "AAAA", ipv6address)
-		} else {
-			log.Debugf("Ipv6 update not required for %s.%s", record.Host, record.Domain)
-		}
+		updateRecord(ctx, record, client, ipv4address, ipv6address)
 	}
 
 	if err != nil {
 		log.Error(err)
+	}
+}
+
+func updateRecord(ctx context.Context, record Record, client *porkbun.Client, ipv4address string, ipv6address string) {
+	ipv4Record, ipv6Record, err := getRecords(ctx, record.Domain, record.Host, client)
+	if err != nil {
+		log.Error(err)
+	}
+	if record.IpV4 && ipv4Record != nil && ipv4address != "" && ipv4Record.Content != ipv4address {
+		err = createOrUpdateRecord(ctx, client, ipv4Record.ID, record.Domain, record.Host, "A", ipv4address)
+	} else {
+		log.Debugf("Ipv4 update not required for %s.%s", record.Host, record.Domain)
+	}
+	if record.IpV6 && ipv6Record != nil && ipv6address != "" && ipv6Record.Content != ipv6address {
+		err = createOrUpdateRecord(ctx, client, ipv6Record.ID, record.Domain, record.Host, "AAAA", ipv6address)
+	} else {
+		log.Debugf("Ipv6 update not required for %s.%s", record.Host, record.Domain)
 	}
 }
